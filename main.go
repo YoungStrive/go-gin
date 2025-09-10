92bd71f83fc083a4c183cfde45e571c0650f8307
YoungStrive <ylstrive@163.com>
2025-09-10 21:52:02 +0800

    2025-09-10 go-gin 学习代码提交

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+type LoginInfo struct {
+	Account string `form:"account"`
+	Pwd     string `form:"pwd"`
+	//多个逗号分隔
+	Email string `json:"email" binding:"required,email"`
+}
+
+// 中间件1
+func mw1() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("mw1 before")
+		c.Next()
+		fmt.Println("mw1 after")
+	}
+}
+func mw2() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("mw2 before")
+		c.Next()
+		fmt.Println("mw2 after")
+	}
+}
 func main() {
 	//路由
 	router := gin.Default()
@@ -47,6 +70,63 @@ func main() {
 	router.Static("/static", "./static")
 	//不显示文件名
 	router.StaticFile("/f1", "./static/1.text")
+
+	//需要先加载模板
+	router.LoadHTMLGlob("templates/**/*")
+
+	router.GET("/index1", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "top/index.impl", gin.H{"title": "我是首页！"})
+	})
+
+	//占位的
+	router.GET("/index/:u/:a", func(c *gin.Context) {
+		name := c.Param("u")
+		age := c.Param("a")
+		c.JSON(200, gin.H{
+			"name": name,
+			"age":  age,
+		})
+
+	})
+	router.GET("/index?name=u1&age=a1", func(c *gin.Context) {
+		name := c.Param("name")
+		age := c.Param("age")
+		c.JSON(200, gin.H{
+			"name": name,
+			"age":  age,
+		})
+
+	})
+	//post  form-data
+	router.POST("/login", func(c *gin.Context) {
+		a := c.PostForm("account")
+		p := c.PostForm("pwd")
+		c.JSON(200, gin.H{
+			"account": a,
+			"pwd":     p,
+		})
+	})
+
+	//post  json 结构体获取
+	router.POST("/loginJson", func(c *gin.Context) {
+		info := &LoginInfo{}
+		err := c.ShouldBind(info)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, info)
+	})
+
+	//测试中间件
+	router.GET("/testNw", mw1(), mw2(), func(c *gin.Context) {
+		fmt.Println("我是接口的")
+		c.String(200, "Hello MY IS zj")
+
+	})
 	port := ":8090"
 	router.Run(port)
 }
